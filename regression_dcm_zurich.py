1bcc0bc3d629b00c721d41d902477a1472e93d23
etdufay <etiennedufayet@outlook.com>
2023-05-26 15:22:08 -0400

    Log-regression; with outputs and several scores

--- regression_dcm_zurich.py
+++ regression_dcm_zurich.py
@@ -5,9 +5,8 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import SequentialFeatureSelector
-from sklearn.metrics import roc_auc_score
 
-
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 path_to_csv = '/Users/etiennedufayet/Desktop/STAGE_3A/Compression_detection_zurich/dataset_zurich_metrics.csv'
 
@@ -31,13 +30,13 @@ y = y.reset_index(drop=True)
 # Add a constant column for the intercept term
 X['intercept'] = 1
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, shuffle=True)
 
 
 model = LogisticRegression()
 
 # Perform forward stepwise selection
-selector = SequentialFeatureSelector(model, direction='forward')
+selector = SequentialFeatureSelector(model, direction='forward', n_features_to_select='auto', tol=None)
 selector.fit(X_train, y_train)
 
 # Get the selected feature indices
@@ -47,6 +46,35 @@ params = selector.get_params(deep=True)
 
 selected_features = X_train.columns[selected_feature_indices]
 
+
 # Print the selected features and params
+'''
 print("Selected Features:", selected_features)
 print(params)
+'''
+
+
+# adjust columns with respect to feature selection
+X_train = X_train.drop(columns = selected_features)
+X_test = X_test.drop(columns = selected_features)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+
+
+cm = confusion_matrix(y_test, y_pred)
+print("Matrice de confusion:")
+print(cm)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Exactitude:", accuracy)
+
+precision = precision_score(y_test, y_pred)
+print("Précision:", precision)
+
+recall = recall_score(y_test, y_pred)
+print("Rappel:", recall)
+
+auc_roc = roc_auc_score(y_test, y_pred)
+print("AUC-ROC:", auc_roc)
